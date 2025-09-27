bf09649dcf82f7d017d6c5ae26d162daf23091b9
Kayky de Brito <oss@kaykyb.com>
2025-09-27 00:30:18 -0300

    Do not deref null on add_employee

--- src/parse.c
+++ src/parse.c
@@ -73,6 +73,11 @@ void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees)
 
 int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring)
 {
+  if (dbhdr == NULL || employees == NULL || addstring == NULL)
+  {
+    return STATUS_ERROR;
+  }
+
   char *name = strtok(addstring, ",");
   char *addr = strtok(NULL, ",");
   char *hours = strtok(NULL, ",");
