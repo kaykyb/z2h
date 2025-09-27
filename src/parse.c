5eb5705857cbecfb28513b19125b78e592dbe659
Kayky de Brito <oss@kaykyb.com>
2025-09-27 01:24:50 -0300

    Prevent segfault on list_employees

--- src/parse.c
+++ src/parse.c
@@ -62,6 +62,11 @@ int remove_employee(struct dbheader_t *dbhdr, struct employee_t *employees, stru
 
 void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees)
 {
+  if (dbhdr == NULL || employees == NULL)
+  {
+    return STATUS_ERROR;
+  }
+
   for (int i = 0; i < dbhdr->count; i++)
   {
     printf("Employee %d\n", i);
